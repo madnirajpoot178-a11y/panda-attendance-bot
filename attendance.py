eb236a8d8bfc104076d430a6677911477ffab46f
madnirajpoot178-a11y <madnirajpoot178@gmail.com>
2026-06-25 17:51:53 +0530

    Create attendance.py

---
+++ attendance.py
@@ -0,0 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file(
+    "vocal-affinity-500511-m8-a1b6e071f727.json",
+    scopes=SCOPES
+)
+
+client = gspread.authorize(CREDS)
+
+sheet = client.open("Panda Attendance").sheet1
+
+def save_attendance(username, action, time_now, date_now):
+    sheet.append_row([
+        date_now,
+        username,
+        action,
+        time_now
+    ])
