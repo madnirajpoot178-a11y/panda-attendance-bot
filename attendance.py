7d74a9c8f295dffde3097f133b6185cf1c61aa1a
madnirajpoot178-a11y <madnirajpoot178@gmail.com>
2026-06-25 20:26:43 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -9,7 +9,6 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Read credentials from Railway Variable
 google_creds = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
 
 CREDS = Credentials.from_service_account_info(
@@ -19,29 +18,112 @@ CREDS = Credentials.from_service_account_info(
 
 client = gspread.authorize(CREDS)
 
-# Your Google Sheet Name
 sheet = client.open("Panda Attendence Sheet").sheet1
 
 
-# Store user data in memory
-user_sessions = {}
+def add_record(date_now, username, action, time_now,
+               break_type="", minutes=""):
 
-
-def save_attendance(username, action, time_now, date_now):
     sheet.append_row([
         date_now,
         username,
         action,
-        time_now
+        time_now,
+        break_type,
+        minutes
     ])
 
 
-def get_user_session(username):
-    if username not in user_sessions:
-        user_sessions[username] = {
-            "start_work": None,
-            "break_start": None,
-            "total_break_minutes": 0
-        }
+def get_all_records():
+    return sheet.get_all_records()
 
-    return user_sessions[username]
+
+def get_user_today_records(username, date_now):
+
+    rows = get_all_records()
+
+    result = []
+
+    for row in rows:
+
+        if (
+            str(row["Date"]) == str(date_now)
+            and str(row["Username"]) == str(username)
+        ):
+            result.append(row)
+
+    return result
+
+
+def get_last_open_break(username, date_now):
+
+    records = get_user_today_records(
+        username,
+        date_now
+    )
+
+    break_type = None
+
+    for row in records:
+
+        if row["Action"] == "Break Start":
+            break_type = row["Break Type"]
+
+        elif row["Action"] == "Break End":
+            break_type = None
+
+    return break_type
+
+
+def get_start_work_time(username, date_now):
+
+    records = get_user_today_records(
+        username,
+        date_now
+    )
+
+    for row in records:
+
+        if row["Action"] == "Start Work":
+            return row["Time"]
+
+    return None
+
+
+def get_break_totals(username, date_now):
+
+    records = get_user_today_records(
+        username,
+        date_now
+    )
+
+    smk_wc = 0
+    lunch = 0
+    dinner = 0
+
+    for row in records:
+
+        if row["Action"] != "Break End":
+            continue
+
+        try:
+            minutes = int(row["Minutes"])
+        except:
+            minutes = 0
+
+        break_type = row["Break Type"]
+
+        if break_type in ["SMK", "WC"]:
+            smk_wc += minutes
+
+        elif break_type == "Lunch":
+            lunch += minutes
+
+        elif break_type == "Dinner":
+            dinner += minutes
+
+    return {
+        "smk_wc": smk_wc,
+        "lunch": lunch,
+        "dinner": dinner
+    }
